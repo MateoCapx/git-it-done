bd835e0b6bdec0bfb02910573e1c00f3bf9a5f51
Mateo <mateom.realtor@gmail.com>
2022-05-22 18:17:39 -0400

    Add another page for repo

--- sandbox.js
+++ sandbox.js
@@ -12,20 +12,20 @@ let getUserRepos = function (user) {
         // 200 Status Code  = ok
         // 404 Status Code = issue
         if (response.ok) {
-        response.json().then(function (data) {
-            displayRepos(data, user);
-        });
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
 
-    } else{
-        alert("Error: GitHub User Not Found");
-    }
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
     })
         // If connectivity issues arise. if the request fails, that error will be sent to the .catch() method
-    .catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
-      });
-    
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
+        });
+
 };
 
 getUserRepos()
@@ -57,30 +57,31 @@ let displayRepos = function (repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
 
+    let repoContainerEl = document.querySelector("#repos-container");
+    let repoSearchTerm = document.querySelector("#repo-search-term");
+
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
 
-    var repoContainerEl = document.querySelector("#repos-container");
-    var repoSearchTerm = document.querySelector("#repo-search-term");
 
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
-    for (var i = 0; i < repos.length; i++) {
+    for (let i = 0; i < repos.length; i++) {
         // format repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        let repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        let repoEl = document.createElement("div");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
 
         // create a span element to hold repository name
-        var titleEl = document.createElement("span");
+        let titleEl = document.createElement("span");
         titleEl.textContent = repoName;
 
         // append to container
@@ -88,7 +89,7 @@ let displayRepos = function (repos, searchTerm) {
 
 
         // create a status element
-        var statusEl = document.createElement("span");
+        let statusEl = document.createElement("span");
         statusEl.classList = "flex-row align-center";
 
         // check if current repo has issues or not
@@ -97,10 +98,10 @@ let displayRepos = function (repos, searchTerm) {
         // If there are no issues, we'll display a blue check mark instead.
 
         if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>"; // IS an icon from a website called "Font Awesome"
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>"; // IS an icon from a website called "Font Awesome"
         }
 
         // append to container
